5b10064a21b2100957865c463cb42e0e497052b7
Marcus <marcusjw@zoho.com>
2024-06-25 16:17:44 -0700

    changed correlation code

--- src/eval_pm/correlations.py
+++ src/eval_pm/correlations.py
@@ -7,7 +7,7 @@ import seaborn as sns
 from random import randint
 # Load predictions from a file
 train_test = "train"
-split = "harmless-base"
+
 mapping = {"harmless-base": (0,42537),
             "helpful-base": (42538, 86372),
             "helpful-online": (86373, 108379),
@@ -15,10 +15,10 @@ mapping = {"harmless-base": (0,42537),
 # "harmless-base", "helpful-base", "helpful-online", "helpful-rejection-sampled"
 def load_perspective_predictions(in_filepath):
     with open(in_filepath, 'r', encoding='utf-8') as in_file:
-        start = mapping[split][0]
-        stop = mapping[split][1]
-        part = [line for i, line in enumerate(in_file) if start <= i <= stop]
-        preds = [float(json.loads(line)["logits_chosen"]) > float(json.loads(line)["logits_rejected"]) for line in part]
+        #start = mapping[split][0]
+        #stop = mapping[split][1]
+        #part = [line for i, line in enumerate(in_file) if start <= i <= stop]
+        preds = [float(json.loads(line)["logits_chosen"]) > float(json.loads(line)["logits_rejected"]) for line in in_file]
         #preds = [randint(0,1) for line in in_file]
     return np.array([1 if pred else 0 for pred in preds])
 
@@ -38,14 +38,11 @@ def calculate_agreement_proportions(filepaths):
         agreement_matrix[-1, i] = agreement_matrix[i, -1]
         agreement_matrix[-1, -1] = 1
     # Mapping filepaths to a simpler name
-    simplified_names = ["1 imagining 1", "1 imagining 3", "3 imagining 1", "3 imagining 3"]
-    simplified_names.append(split)
+    simplified_names = ["1st", "3rd", "hh-rlhf"]
     return agreement_matrix, simplified_names
 
-base = f"data/hh_labels/4_{split}_{train_test}_"
+base = f"data/hh_labels/4_{train_test}_"
 filepaths = [
-    base + "1_1.jsonl",
-    base + "1_3.jsonl",
     base + "3_1.jsonl",
     base + "3_3.jsonl",
     #"data/datasets/hh-rlhf-train-extracted.jsonl",
